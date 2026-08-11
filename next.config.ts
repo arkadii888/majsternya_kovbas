e73d645092fb0a9991c6ec6f3f33c68b37f0b621
Arkadii <arkadii.personal@proton.me>
2026-08-11 19:09:45 +0200

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/majsternya_kovbas'
 };
 
 export default nextConfig;
